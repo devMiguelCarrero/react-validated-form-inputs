95f72e9d4d0f3dde0cf2bc19101d4be00c625713
Miguel.Carrero <Miguel.Carrero@gaincapital.com>
2024-01-02 22:25:13 -0500

    add main configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, context) => {
+    // Enable polling based on env variable being set
+    if (process.env.NEXT_WEBPACK_USEPOLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300,
+      };
+    }
+    config.resolve.fallback = { fs: false, module: false, path: false };
+    return config;
+  },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
